729883708b7200299718c04adabd0b6cdb48ab41
Peter DiPalma <pdipalm@gmail.com>
2022-09-27 15:55:57 -0400

    swapped intake and burn colors in DashChart
    the fact that intake was red and burn was blue was irritating the hell out of me

--- src/dash/DashChart.js
+++ src/dash/DashChart.js
@@ -1,65 +1,73 @@
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import faker from 'faker';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import faker from "faker";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 export const options = {
-    responsive: true,
-    maintainAspectRatio: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart',
-        },
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: "top",
     },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
 };
 
-const labels = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const labels = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Calorie Intake',
-            data: labels.map(() => faker.datatype.number({ min: 500, max: 4000 })),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        },
-        {
-            label: 'Calorie Burn',
-            data: labels.map(() => faker.datatype.number({ min: 500, max: 3000 })),
-            borderColor: 'rgb(30, 99, 200)',
-            backgroundColor: 'rgb(30, 99, 250)',
-        },
-    ],
+  labels,
+  datasets: [
+    {
+      label: "Calorie Intake",
+      data: labels.map(() => faker.datatype.number({ min: 500, max: 4000 })),
+      borderColor: "rgb(30, 99, 200)",
+      backgroundColor: "rgb(30, 99, 250)",
+    },
+    {
+      label: "Calorie Burn",
+      data: labels.map(() => faker.datatype.number({ min: 500, max: 3000 })),
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
 };
 
 export function LineChart() {
-    return (
-        <Container>
-            <Line options={options} data={data} />
-        </Container>
-    )
+  return (
+    <Container>
+      <Line options={options} data={data} />
+    </Container>
+  );
 }
