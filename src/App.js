c3f138f9c9c2d7dd22b41f8d78a0b514f989d4c4
joeysaline <jsaline@albany.edu>
2022-10-18 18:16:22 -0400

    update app  and landing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "./user-auth/contexts/AuthContexts";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Landing from './Landing'
 import Dashboard from './Dashboard'
 import SignUp from "./user-auth/SignUp"
 import LogIn from "./user-auth/LogIn"
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}>
-
-      <div className='w-100' style={{ maxWidth: "400px" }}>
-        HEY COACH
-        <Router>
-          <AuthProvider>
-            <Routes>
-              <Route exact path='/' element={<Dashboard />} />
-              <Route path='/signup' element={<SignUp />} />
-              <Route path='/login' element={<LogIn />} />
-            </Routes>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route exact path='/' element={<Landing />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<LogIn />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   )
 }
 
