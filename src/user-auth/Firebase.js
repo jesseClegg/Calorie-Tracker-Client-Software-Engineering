3b5252ef2254549e2653a5061e5fb58fe97cff5d
joeysaline <jsaline@albany.edu>
2022-10-18 17:20:46 -0400

    fixed firebase bug

--- src/user-auth/Firebase.js
+++ src/user-auth/Firebase.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
+//console.log(process.env);
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()
