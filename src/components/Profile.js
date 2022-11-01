fa60dc35a6d4592ad3794dccf7c7e383cdac657a
joeysaline <jsaline@albany.edu>
2022-11-01 18:31:50 -0400

    update profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useAuth } from '../user-auth/contexts/AuthContexts'
 
 export default function Profile() {
+
+    const { currentUser } = useAuth();
     return (
-        <div>Profile page yo</div>
+        <>
+            <div>Profile page yo</div>
+            <h3>Welcome {currentUser.email}</h3>
+        </>
     )
 }
