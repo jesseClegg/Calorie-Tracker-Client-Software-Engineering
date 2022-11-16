38a0520b18eccaf77bcf1c6b60ad8190a580f681
Peter DiPalma <pdipalm@gmail.com>
2022-11-15 20:35:23 -0500

    added requests to main, activity.js and nutrition.js
    user account added to mongo when seen on dash. Activity and Nutrition print json objects from backend

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../user-auth/contexts/AuthContexts";
+import axios from "axios";
 
-export default function Activity() {
-    return (
-        <div>Activity page yo</div>
-    )
+export default function Nutrition() {
+  const { currentUser } = useAuth();
+  const [activities, setActivities] = useState(
+    getActivities(currentUser.email)
+  );
+
+  function getActivities(userEmail) {
+    axios
+      .request({
+        method: "GET",
+        url: `http://localhost:3000/api/getAllActivities`,
+        data: {
+          email: userEmail,
+        },
+      })
+      .then(function (response) {
+        setActivities(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        setActivities("error");
+      });
+  }
+  //   console.log(meals);
+  return <div>{activities}</div>;
 }
